24e7765d7257a7c9908d990233e3bff42b5730d4
sergiommc <sergiomatosmoreira@hotmail.com>
2014-10-29 20:44:21 -0300

    Create ControleAcademico.java

---
+++ ControleAcademico.java
@@ -0,0 +1,21 @@
+LPT2
+====
+
+AULAS
+package Controle;
+import Academico.*;
+
+
+import java.util.ArrayList;
+
+public class ControleAcademico {
+	private ArrayList<Disciplina> listaDisciplina = new ArrayList <Disciplina>(); 
+	private String disciplinasCadastradas;
+	
+	public void setAdicionarDisciplina(Disciplina disc){
+		listaDisciplina.add(disc);
+	}
+	public void setAdicionarAlunoDisciplina(int cod_disciplina, Aluno aluno){
+		
+	}
+}
